7f62f03827f86b061d1fccac2cd76d36f656fa0e
jojcurr <curr0250@algonquinlive.com>
2017-10-17 12:10:38 -0400

    final change

--- namespace.js
+++ namespace.js
@@ -2,24 +2,25 @@ var CURR0250 = {
     init: function () {
         console.log("script in CURR0250 called");
         var div = document.createElement("div");
-        
+      
 
         div.className = "box";
         div.textContent = "CURR0250";
 
         document.getElementById("boxes").appendChild(div);
 
-        div.addEventListener("click", click_me);
-        div.addEventListener("mouseover", toggleHighlight);
-        div.addEventListener("mouseout", toggleHighlight);
+        div.addEventListener("click", click);
+        div.addEventListener("mouseover", hover);
+        div.addEventListener("mouseout", hover);
 
-        function toggleHighlight(ev) {
+        function hoverMe(ev) {
             this.classList.toggle("highlight");
         }
 
-        function click_me(ev) {
-                this.style.backgroundColor = "pink";
-                this.style.borderColor = "blue";
+        function clickMe(ev) {
+
+                this.style.backgroundColor = "#d742f4";
+                this.style.borderColor = "#41a0f4";
             }
 
         }
